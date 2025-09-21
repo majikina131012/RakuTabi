d6fbcfb0916a2bafc7c4049575a6f73313072c18
EC2 Default User <ec2-user@ip-172-31-12-221.ap-northeast-1.compute.internal>
2025-09-21 07:08:38 +0000

    同じ人の投票は更新

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -5,6 +5,8 @@ class VotesController < ApplicationController
     @vote = @group.votes.new
     @day = params[:day]
     @time = params[:time]
+    @calendar_type = params[:view] || "week"
+    @existing_users = @group.users
   end
   
   # def create
@@ -21,33 +23,34 @@ class VotesController < ApplicationController
   # end
 
   def create
-    group = Group.find(params[:group_id])
-
-    # 入力された名前でユーザーを取得 or 作成
-    user = group.users.find_or_create_by(name: params[:user_name])
-
-    # daysパラメータの形: {"2025-09-20"=>"○", "2025-09-21"=>"✕", "2025-09-22"=>"△"}
-    days = params[:days] || {}
-
-    success = true
-    days.each do |day, status|
-      vote = group.votes.new(
-        user_id: user.id,
-        group_id: group.id,
-        day: day,
-        status: status
-      )
-      success &&= vote.save
+    @group = Group.find(params[:group_id])
+  
+    # 新規ユーザー名
+    new_user_name = params[:user_name].to_s.strip
+    new_user = nil
+    if new_user_name.present?
+      # グループ内で重複しないようスコープ付きで検索・作成
+      new_user = @group.users.find_or_create_by(name: new_user_name)
     end
-
-    if success
-      redirect_to group_votes_path(group.id), notice: "投票を登録しました"
-    else
-      flash.now[:alert] = "投票の保存に失敗しました"
-      @group = group
-      render :new
+  
+    # 既存ユーザー選択
+    existing_users = @group.users.where(id: params[:existing_user_ids])
+  
+    # 投票対象ユーザーを一意にまとめる
+    users_to_vote = ([new_user] + existing_users.to_a).compact.uniq { |u| u.id }
+  
+    users_to_vote.each do |user|
+      params[:days].each do |day, status_str|
+        # 同じ日付・同じユーザーの投票が存在しなければ作成、既存ならstatusを更新
+        vote = @group.votes.find_or_initialize_by(user: user, day: day)
+        vote.status = status_str
+        vote.save!
+      end
     end
+  
+    redirect_to group_votes_path(@group), notice: "投票を登録しました"
   end
+  
 
   def index
     @group = Group.find(params[:group_id])
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   belongs_to :group
   has_many :votes
 
-  validates :name, presence: true
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: { scope: :group_id }
   validates :group_id, presence: true
 
   
