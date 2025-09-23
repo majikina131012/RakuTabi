7daa4e68a9093f967319cf35fe00feb9a5048759
EC2 Default User <ec2-user@ip-172-31-12-221.ap-northeast-1.compute.internal>
2025-09-23 14:03:15 +0000

    updateアクションの削除

--- app/controllers/item_checks_controller.rb
+++ app/controllers/item_checks_controller.rb
@@ -1,14 +1,5 @@
 class ItemChecksController < ApplicationController
 
-  def update
-    @item_check = ItemCheck.find_by(item_id: params[:item_id], user_id: params[:user_id])
-    if is_ok_value = params[:is_ok] == "true" || params[:is_ok] == "1"
-      @item_check.update(is_ok: true)
-      @item_check.update(is_ok: is_ok_value)
-    end
-    redirect_to group_items_path(params[:group_id])
-  end
-  
   def bulk_update
     # params[:item_checks] が [{"id"=>"3"}, {"id"=>"4"}] の形式なので id を取り出す
     checked_ids = params[:item_checks].map { |h| h[:id].to_i }
