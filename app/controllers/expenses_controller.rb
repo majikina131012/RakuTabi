8bf04215e09254a1d1dc91fbb0b13949b6336f10
EC2 Default User <ec2-user@ip-172-31-12-221.ap-northeast-1.compute.internal>
2025-10-23 12:08:50 +0000

    精算編集修正

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -60,8 +60,41 @@ class ExpensesController < ApplicationController
   
   def update
     @group = Group.find(params[:group_id])
-    @expense =  Expense.find(params[:id])
+    @expense = @group.expenses.find(params[:id])
+    user_ids = params[:expense][:user_ids].reject(&:blank?).map(&:to_i)
+  
     if @expense.update(expense_params)
+      # 既存の shares を全削除
+      @expense.shares.destroy_all
+  
+      # 割り勘計算
+      if user_ids.any?
+        share_amount = @expense.amount / user_ids.size.to_f
+  
+        # 受取人の shares を作成
+        user_ids.each do |user_id|
+          @expense.shares.create!(
+            user_id: user_id,
+            must_pay: share_amount,
+            pay: 0,
+            pay_to_user_id: @expense.payer_id == user_id ? nil : @expense.payer_id
+          )
+        end
+      end
+  
+      # payer 用の share を作成（または更新）
+      unless user_ids.include?(@expense.payer_id)
+        @expense.shares.create!(
+          user_id: @expense.payer_id,
+          must_pay: 0,
+          pay: @expense.amount,
+          pay_to_user_id: nil
+        )
+      else
+        payer_share = @expense.shares.find_by(user_id: @expense.payer_id)
+        payer_share.update!(pay: @expense.amount) if payer_share && payer_share.pay.to_f == 0
+      end
+  
       flash[:notice] = "変更しました"
       redirect_to group_path(@group.id)
     else
