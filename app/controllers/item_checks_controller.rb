96d28e98f476a92020ef0f3acb643bcb2ff9c8fe
EC2 Default User <ec2-user@ip-172-31-12-221.ap-northeast-1.compute.internal>
2025-10-29 12:49:37 +0000

    チェックなしでの更新エラーの修正

--- app/controllers/item_checks_controller.rb
+++ app/controllers/item_checks_controller.rb
@@ -1,17 +1,19 @@
 class ItemChecksController < ApplicationController
 
   def bulk_update
-    # params[:item_checks] が [{"id"=>"3"}, {"id"=>"4"}] の形式なので id を取り出す
+    group = Group.find(params[:group_id])
+  
+    if params[:item_checks].blank?
+      redirect_to group_items_path(group), notice: "何も選択されていません"
+      return
+    end
+  
     checked_ids = params[:item_checks].map { |h| h[:id].to_i }
   
-    # グループ内の全 ItemCheck を false にリセット
-    group = Group.find(params[:group_id])
     ItemCheck.where(item_id: group.items.pluck(:id)).update_all(is_ok: false)
-  
-    # チェックされたものだけ true に更新
     ItemCheck.where(id: checked_ids).update_all(is_ok: true)
   
-    redirect_to group_items_path(group)
+    redirect_to group_items_path(group), notice: "チェックを更新しました"
   end
 
   private
